efb707a2f6df5b1dd969a8dd5fbda4434b364289
kwucslkm <kwucsani@naver.com>
2025-10-03 22:06:19 +0900

    20251003 3th commit

--- project/2_weapon_keyevent.py
+++ project/2_weapon_keyevent.py
@@ -38,17 +38,26 @@ character_size = character.get_rect().size
 character_width = character_size[0]
 character_height = character_size[1]
 character_x = screen_width /2  - character_width / 2
-character_y = screen_height- stage_height - character_height
+character_y = screen_height - stage_height - character_height
 
 # 캐릭터 이동 
 to_x = 0
 # to_y = 0
 
 # 이동 속도
-character_speed = 5
+character_speed = 0.6
 weapon_speed = 10
 # 무기
+weapons = []
 weapon = pygame.image.load(os.path.join(image_path, "weapon.png"))
+weapon_size = weapon.get_rect().size
+weapon_width = weapon_size[0]
+weapon_height = weapon_size[1]
+weapon_x = character_x + character_width/2 - weapon_width/2
+weapon_y = screen_height - stage_height
+# 무기 이동
+
+to_weapon = 0
 # 볼 가져오기 
 
 # 폰트 정의
@@ -61,7 +70,7 @@ weapon = pygame.image.load(os.path.join(image_path, "weapon.png"))
 running = True # 게임이 진행중인가?
 
 while running:
-    dt  = clock.tick(60) # 게임화면의 초당 프레임 수를 설정
+    dt  = clock.tick(30) # 게임화면의 초당 프레임 수를 설정
     # print("fps : " + str(clock.get_fps()))
     for event in pygame.event.get(): # 어떤 이벤트가 발생하였는가?
         if event.type == pygame.QUIT: # 창이 닫히는 이벤트가 발생하였는가?
@@ -73,27 +82,40 @@ while running:
             elif event.key == pygame.K_RIGHT: # 캐릭터를 오른쪽으로
                 to_x += character_speed
             elif event.key == pygame.K_SPACE: # 무기 발사
-                
-                to_weapon += weapon_speed
+                weapon_x = character_x + character_width/2 - weapon_width/2
+                weapon_y = screen_height - stage_height
+                weapons.append([weapon_x, weapon_y])
+
             # elif event.key == pygame.K_UP: # 캐릭터를 위로
             #     to_y -= character_speed
             # elif event.key == pygame.K_DOWN: # 캐릭터를 아래로
             #     to_y += character_speed
-    # 가로 경계값 처리
+        if event.type == pygame.KEYUP: # 방향키를 떼면 멈춤
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                to_x = 0
+    
+    # 케릭터 위치 정의
 
+    character_x += to_x * dt
+    # 가로 경계값 처리    # 세로 경계값 처리
+    if character_x < 0:
+        character_x = 0
+    elif character_x > screen_width - character_width:
+        character_x = screen_width - character_width
+    # 무기 위치 정의
+    weapons = [[w[0], w[1] - weapon_speed] for w in weapons] # 무기 위치를 위로 올림
 
-    # 세로 경계값 처리
-
+    # 천장에 닿은 무기 없애기
+    weapons = [ [w[0], w[1]] for w in weapons if w[1] > 0]
         
     # 충돌 처리를 위한 rect 정보 업데이트
 
-
     # 충돌 체크
 
-
-    # screen.blit(background, (0, 0)) # 배경 그리기
     # screen.fill((0, 0, 255)) # 배경을 파란색으로 채우기
-    screen.blit(background, (0, 0))
+    screen.blit(background, (0, 0)) # 배경 그리기
+    for weapon_x, weapon_y in weapons:
+        screen.blit(weapon, (weapon_x, weapon_y))
     screen.blit(stage, (0, screen_height - stage_height))
     screen.blit(character, (character_x, character_y))
 
@@ -106,8 +128,6 @@ while running:
     # 출력할 글자, True, 글자 색상
 
     # 시간이 0 이하이면 게임 종료
-
-
 
     pygame.display.update() # 화면 업데이트
 
